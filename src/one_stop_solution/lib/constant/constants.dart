6a6dafebb70acefc4e49060f6ec26ad3b438f9d1
Kapoor-0905 <500097111@stu.upes.ac.in>
2023-03-23 20:20:33 +0530

    Firebase integration

---
+++ src/one_stop_solution/lib/constant/constants.dart
@@ -0,0 +1,9 @@
+class constants {
+  static const String apiKey = "AIzaSyAXSTWSHC7Ta-zxUrE1Mo1laOeIhcOD0I0";
+  static const String authDomain = "one-stopsolution.firebaseapp.com";
+  static const String projectId = "one-stopsolution";
+  static const String storageBucket = "one-stopsolution.appspot.com";
+  static const String messagingSenderId = "806062598082";
+  static const String appId = "1:806062598082:web:38145a38f2b787fd50185b";
+  static const String measurementId = "G-5HBTKEYNWN";
+}
